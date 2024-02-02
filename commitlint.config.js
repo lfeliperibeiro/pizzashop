8af6cc9df6ac6d3d40b0d80b00108da78c994aea
Felipe Ribeiro <piporibeiro@gmail.com>
2024-02-01 21:12:04 -0300

    change comitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,20 @@
-import '@commitlint/config-conventional'
+const config = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore'],
+    ],
+    'type-case': [2, 'always', 'lower-case'],
+    'subject-case': [
+      2,
+      'never',
+      ['sentence-case', 'start-case', 'pascal-case', 'upper-case'],
+    ],
+    'subject-empty': [2, 'never'],
+    'header-max-length': [2, 'always', 72],
+  },
+}
+
+export default config
